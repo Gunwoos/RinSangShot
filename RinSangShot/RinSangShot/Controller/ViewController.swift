bc9ee0fda244ff8a5457aa80059f36cb841b4ce1
Gun woo Lim <lkwyoyo@gmail.com>
2019-01-23 19:05:48 +0900

    update

---
+++ RinSangShot/RinSangShot/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RinSangShot
+//
+//  Created by 임건우 on 23/01/2019.
+//  Copyright © 2019 lims. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RinSangShot/RinSangShot/View/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  RinSangShot
+//
+//  Created by 임건우 on 23/01/2019.
+//  Copyright © 2019 lims. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
