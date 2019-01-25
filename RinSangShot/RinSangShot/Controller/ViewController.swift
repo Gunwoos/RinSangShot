d18b67cb181a221e5126eb99f0a13485dda341a4
Gun woo Lim <lkwyoyo@gmail.com>
2019-01-25 18:17:33 +0900

    Basic UI Update

--- RinSangShot/RinSangShot/Controller/ViewController.swift
+++ RinSangShot/RinSangShot/Controller/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TopView: UIView!
+    @IBOutlet weak var SettingButton: UIButton!
+    @IBOutlet weak var ViewSizeButton: UIButton!
+    @IBOutlet weak var ChangeButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var BottomView: UIView!
+    @IBOutlet weak var AlbumButon: UIButton!
+    @IBOutlet weak var TakeButton: UIButton!
+    @IBOutlet weak var FilterButton: UIButton!
+    
+    @IBOutlet weak var CameraImageVIew: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ RinSangShot/RinSangShot/View/HomeViewController.swift
@@ -0,0 +1,11 @@
+//
+//  HomeViewController.swift
+//  RinSangShot
+//
+//  Created by 임건우 on 25/01/2019.
+//  Copyright © 2019 lims. All rights reserved.
+//
+
+import UIKit
+
+
