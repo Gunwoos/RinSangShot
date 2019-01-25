acf2fb058b62117039e3295d5e1956d4674e3b24
Gun woo Lim <lkwyoyo@gmail.com>
2019-01-25 20:01:31 +0900

    take album

--- RinSangShot/RinSangShot/Controller/ViewController.swift
+++ RinSangShot/RinSangShot/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
@@ -23,15 +24,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var FilterButton: UIButton!
     
     @IBOutlet weak var CameraImageVIew: UIImageView!
-    
-    
-    
+    private let imagePickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePickerController.delegate = self
+        
     }
+    
+    @IBAction func ShowAlbum(_ sender: Any) {
+        print("\n---------- [ pickImage ] ----------\n")
+        let type = UIImagePickerController.SourceType.photoLibrary
+        guard UIImagePickerController.isSourceTypeAvailable(type) else { return }
+        
+        imagePickerController.sourceType = type
+        present(imagePickerController, animated: true)
+    }
+    
 
 
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Image Picker Did Pick")
+        
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+        if UTTypeEqual(mediaType, kUTTypeMovie){
+            let originalVideo = info[UIImagePickerController.InfoKey.mediaURL] as! NSURL
+            
+            if let path = originalVideo.path{
+                UISaveVideoAtPathToSavedPhotosAlbum(path, nil, nil, nil)
+            }
+        }
+        else{
+            let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            let selectedImage = editedImage ?? originalImage
+            CameraImageVIew.image = selectedImage
+            
+            if let saveImage = selectedImage{
+                UIImageWriteToSavedPhotosAlbum(saveImage, nil, nil, nil)
+            }
+        }
+        
+        picker.dismiss(animated: true)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Image Picker Did Cancel")
+        
+        picker.dismiss(animated: true)
+    }
 }
 
